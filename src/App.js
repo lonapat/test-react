d2566842829e6fd15c6b8df1cb4b91e85b0916dd
lonapat <patrick.loyem@gmail.com>
2022-06-23 18:17:47 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          PATRICK THEODORE
         </a>
       </header>
     </div>
